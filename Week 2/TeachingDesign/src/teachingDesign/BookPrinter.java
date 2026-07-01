bf330488335f431ffae00d949e427f5d81d12e5b
Saket Ranjan <161413274+Saket22Ranjan@users.noreply.github.com>
2026-07-01 15:29:10 +0530

    Learning SRP(Single Responsibilty Principle) by creating TeachingDesign Project

---
+++ Week 2/TeachingDesign/src/teachingDesign/BookPrinter.java
@@ -0,0 +1,11 @@
+package teachingDesign;
+
+public class BookPrinter {
+    void printTextToConsole(Book book) {
+       
+    }
+    void printTextToAnotherMedium(Book book) {
+        
+    }
+    
+}
---
+++ Week 2/TeachingDesign/src/teachingDesign/MainTest.java
@@ -0,0 +1,12 @@
+package teachingDesign;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Book book = new Book();
+        BookPrinter printer = new BookPrinter();
+        System.out.println("Enter the name of the book: ");
+
+        
+    }
+    
+}
