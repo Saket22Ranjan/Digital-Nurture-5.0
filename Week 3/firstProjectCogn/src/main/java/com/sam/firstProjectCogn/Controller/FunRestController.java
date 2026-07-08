8f2f20e43abf8746392a8ecf7d6c631d411ac3f0
Saket Ranjan <161413274+Saket22Ranjan@users.noreply.github.com>
2026-07-08 14:35:53 +0530

    Add initial project structure with Maven configuration, Spring Boot application, and basic REST controller

---
+++ Week 3/firstProjectCogn/src/main/java/com/sam/firstProjectCogn/Controller/FunRestController.java
@@ -0,0 +1,10 @@
+package com.sam.firstProjectCogn.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class FunRestController {
+
+	@GetMapping("/")
+	public String sayHello() { return "Hello World!"; }
+
+}
---
+++ Week 3/firstProjectCogn/src/main/java/com/sam/firstProjectCogn/FirstProjectCognApplication.java
@@ -0,0 +1,13 @@
+package com.sam.firstProjectCogn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectCognApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectCognApplication.class, args);
+	}
+
+}
