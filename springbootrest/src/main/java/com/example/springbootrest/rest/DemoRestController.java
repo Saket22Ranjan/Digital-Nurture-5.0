a14f19ca70a149420359324bf71cc320e2aac527
Saket Ranjan <161413274+Saket22Ranjan@users.noreply.github.com>
2026-07-15 11:52:43 +0530

    Add Spring Boot module configuration and update application properties

--- springbootrest/src/main/java/com/example/springbootrest/rest/DemoRestController.java
+++ springbootrest/src/main/java/com/example/springbootrest/rest/DemoRestController.java
@@ -1,17 +1,13 @@
 package com.example.springbootrest.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@RequestMapping("/test")
 public class DemoRestController {
-    
-    @GetMapping("/hello")
+
+    @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
     }
-
 }
